5d8e5bddb517c45ce85fe8c4f7ba723b7d69a54f
23Jm <jannathmarha23@gmail.com>
2024-01-08 21:45:54 +0530

    counter

---
+++ src/components/Counter.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react"
+import "../Counter.css"
+
+const Counter = () => {
+    let count=0;
+    const [currentState,updatedState]=useState(count)
+    const handleClick=()=>{
+        updatedState(currentState +1)
+    }
+
+  return (
+    <div className="counter-parent">
+      <div className="main-div">
+        <h3>Counter</h3>
+        <button onClick={handleClick}>{currentState}</button>
+      </div>
+    </div>
+  );
+}
+
+export default Counter
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "../Counter.css";
+const Header = () => {
+  return (
+    <div className="main-header">
+      <ul>
+        <li>Home</li>
+        <li>Counter</li>
+        <li>About</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Header
